b4666c5d0a804cd1dc541c933d06815ea642ab1c
fabucorsu <fabiensantoni@icloud.com>
2018-11-23 18:54:20 +0100

    start comment

---
+++ CentraleFitnessTek/Session/ViewNFC.swift
@@ -0,0 +1,30 @@
+//
+//  ViewNFC.swift
+//  CentraleFitnessTek
+//
+//  Created by Fabien Santoni on 23/11/2018.
+//  Copyright © 2018 Fabien Santoni. All rights reserved.
+//
+
+import UIKit
+
+class ViewNFC: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CentraleFitnessTek/Social/comment/CommentInfo.swift
@@ -0,0 +1,9 @@
+//
+//  CommentInfo.swift
+//  CentraleFitnessTek
+//
+//  Created by Fabien Santoni on 23/11/2018.
+//  Copyright © 2018 Fabien Santoni. All rights reserved.
+//
+
+import Foundation
